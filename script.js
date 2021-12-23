0497756bd22871081dc3a6197efb193fe0c5c9ec
BuddyBuie <95647025+BuddyBuie@users.noreply.github.com>
2021-12-23 11:05:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,61 @@ $('nav a').click(function(event) {
     }, 500);
     event.preventDefault();
 });
+
+// IMG Slideshow
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 1) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+
+// img scroll
+
+var images = [];
+
+images[0] = ['https://raw.githubusercontent.com/BuddyBuie/April-White-Navigation/main/img/profile.png'];
+images[1] = ['https://assets.codepen.io/122136/girl-photo.jpg'];
+images[2] = ['https://assets.codepen.io/122136/splash-photo.jpg'];
+var index = 0;
+
+function change() {
+  document.getElementById("mainPhoto").src = images[index];
+  if (index == 2) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  setTimeout(change, 5000);
+}
