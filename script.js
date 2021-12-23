4c2d49495d2e7365c551845f84adbe2a8a7f5821
BuddyBuie <95647025+BuddyBuie@users.noreply.github.com>
2021-12-23 11:25:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -35,43 +35,6 @@ $('nav a').click(function(event) {
     event.preventDefault();
 });
 
-// IMG Slideshow
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 1) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
-
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
-    } else {
-        $('.main_h').addClass('open-nav');
-    }
-});
-
-$('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
-    }
-});
-
-// navigation scroll lijepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-});
-
 
 // img scroll
 
@@ -92,3 +55,5 @@ function change() {
 
   setTimeout(change, 5000);
 }
+
+window.onload = change();
